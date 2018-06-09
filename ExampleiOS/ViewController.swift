c0a15b9e5204291ba90da51dcc3b7369bda14e33
keisuke <jam330157@gmail.com>
2018-06-09 20:44:26 +0900

    add UIColor+Hex Convert hexadecimal to UIColor

---
+++ ExampleiOS/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  ExampleiOS
+//
+//  Created by Shichimitoucarashi on 2018/06/09.
+//  Copyright © 2018年 keisuke yamagishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    public var ColorPattern = ["#5AC8FA",
+                               "#FFCC00",
+                               "#FF9500",
+                               "#FF2D55",
+                               "007aff",
+                               "#4cd964",
+                               "FF3B30",
+                               "8E8E93",
+                               "EFEFF4",
+                               "CECED2",
+                               "007AFF",
+                               "5AC8FA",
+                               "F37CFC"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ColorPattern.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = UIColor(hex: ColorPattern[indexPath.row])
+        cell.textLabel?.text = ColorPattern[indexPath.row]
+        
+        return cell
+        
+    }
+    
+}
+
