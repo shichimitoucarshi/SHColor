836416ac926a1639d00666154b9d2d9c62fc7bc6
keisuke <jam330157@gmail.com>
2018-10-21 17:22:19 +0900

    refacoring

--- Pods/SHColor/Source/UIColor+Hex.swift
+++ Pods/SHColor/Source/UIColor+Hex.swift
@@ -17,33 +17,11 @@ extension UIColor {
             h = h.replacingOccurrences(of: "#",with:"")
         }
         
-        var hc = 0
-        var r = ""
-        var g = ""
-        var b = ""
-        
-        let _ = h.map {arg in
-            
-            switch hc {
-                
-            case 0..<2:
-                r.append(arg)
-                break
-            case 2..<4:
-                g.append(arg)
-                break
-            case 4..<6:
-                b.append(arg)
-                break
-            default:
-                print ("DED")
-            }
-            hc += 1
-        }
-        
-        let rv = CGFloat((Int(r, radix: 16) ?? 0)) / 255.0
-        let gv = CGFloat((Int(g, radix: 16) ?? 0)) / 255.0
-        let bv = CGFloat((Int(b, radix: 16) ?? 0)) / 255.0
+        let hs = h.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
+ 
+        let rv = CGFloat((Int(hs[0] + hs[1], radix: 16) ?? 0)) / 255.0
+        let gv = CGFloat((Int(hs[2] + hs[3], radix: 16) ?? 0)) / 255.0
+        let bv = CGFloat((Int(hs[4] + hs[5], radix: 16) ?? 0)) / 255.0
         
         self.init(red: rv, green: gv, blue: bv, alpha: alpha)
     }
