a4d66dfec380071b63c42c030afde29c2199b03c
keisukeYamagishi <jam330257@gmail.com>
2019-05-24 15:23:04 +0900

    version swift 5

---
+++ SHColor/SHColor.h
@@ -0,0 +1,19 @@
+//
+//  SHColor.h
+//  SHColor
+//
+//  Created by Shichimitoucarashi on 5/24/19.
+//  Copyright © 2019 keisuke yamagishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHColor.
+FOUNDATION_EXPORT double SHColorVersionNumber;
+
+//! Project version string for SHColor.
+FOUNDATION_EXPORT const unsigned char SHColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHColor/PublicHeader.h>
+
+
